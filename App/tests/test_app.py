c3c01de4dd75ae3313c2dbad0b59c93414f17c0f
Jalene Is'raell Armstrong <jalene.armstrong@my.uwi.edu>
2024-10-17 14:23:13 -0400

    Test Updates

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -1,18 +1,15 @@
 import os, tempfile, pytest, logging, unittest
-from werkzeug.security import check_password_hash, generate_password_hash
 
 from App.main import create_app
 from App.database import db, create_db
 from App.views.auth import login
-from App.models import Staff, Student, Review
+from App.models import Staff, Student
 from App.controllers import (
     create_staff,
     add_student,
     add_review,
     get_student_json,
     get_student_reviews_json,
-    get_staff,
-    get_student,
     login
 )
 
@@ -32,12 +29,12 @@ def empty_db():
 '''
 class StudentUnitTests(unittest.TestCase):
 
-    # UNIT TEST - #1 ***
+    # UNIT TEST - #1: STUDENT
     def test_unit_01_new_student(self):
         student = Student(816000010, "Zuzu", "Pembleton", "zuzu.pembleton@mail.com")
-        assert student is not None
-    
-    # UNIT TEST - #2
+        assert student.email == "zuzu.pembleton@mail.com"
+
+    # UNIT TEST - #2: STUDENT JSON
     def test_unit_02_new_student_json(self):
         student = Student(816000020, "Finnley", "Fothergill", "finnley.fothergill@mail.com")
         student_json = student.get_json()
@@ -48,157 +45,103 @@ class StudentUnitTests(unittest.TestCase):
                               "reviews": []}, student_json)
 
 class StaffUnitTests(unittest.TestCase):
-        # UNIT TEST - #3 *** #Then this cant be a unit test because you need an admin to create a staff???
-        def test_unit_03_new_staff(self): 
-            #ENSURING A STAFF WITHOUT ADMIN STATUS CAN BE CREATED
-            staff = Staff("Mr.", "James", "Taylor", "james.taylor@mail.com", False, "jamestay", None)
-            assert not staff.is_admin
 
-        # UNIT TEST - #4
-        def test_unit_03_new_staff(self):
-            #ENSURUNG A STAFF WITH ADMIN STATUS CAN BE CREATED
-            admin = Staff("Prof.", "Amelia", "Wilson", "amelia.wilson@mail.com", True, "amelia123", 0)
-            assert admin.is_admin
+    # UNIT TEST - #3: REGULAR STAFF
+    def test_unit_03_new_regular_staff(self): 
+        staff = Staff("Mr.", "James", "Taylor", "james.taylor@mail.com", False, "jamespass", None)
+        assert not staff.is_admin
 
-        # UNIT TEST - #5
-        def test_unit_04_new_staff_json(self):
-            admin = Staff("Ms.", "Isabella", "Anderson", "isabella.anderson@mail.com", True, "isa_bella", None)
-            staff_json = admin.get_json()
-            self.assertDictEqual({"id": admin.id,
-                                  "prefix": "Ms.",
-                                  "firstname": "Isabella",
-                                  "lastname": "Anderson",
-                                  "email": "isabella.anderson@mail.com",
-                                  "is_admin": True,
-                                  "created_by": None}, staff_json)
+    # UNIT TEST - #4: ADMIN STAFF
+    def test_unit_04_new_admin_staff(self):
+        staff = Staff("Prof.", "Amelia", "Wilson", "amelia.wilson@mail.com", True, "ameliapass", None)
+        assert staff.is_admin
 
-        # UNIT TEST - #6
-        def test_unit_05_set_password(self):
-            password = "lucaspass"
-            hashed_password = generate_password_hash(password)
-            staff = Staff("Prof.", "Lucas", "Garcia", "lucas.garcia@mail.com", True, "lucaspass", None)
-            assert staff.password != password
+    # UNIT TEST - #5: STAFF JSON
+    def test_unit_05_new_staff_json(self):
+        new_staff = Staff("Ms.", "Isabella", "Anderson", "isabella.anderson@mail.com", True, "isabellapass", None)
+        staff_json = new_staff.get_json()
+        self.assertDictEqual({"id": new_staff.id,
+                                "prefix": "Ms.",
+                                "firstname": "Isabella",
+                                "lastname": "Anderson",
+                                "email": "isabella.anderson@mail.com",
+                                "is_admin": True,
+                                "created_by": None}, staff_json)
 
-        # UNIT TEST - #7
-        def test_unit_06_check_password(self):
-            password = "henrypass"
-            staff = Staff("Mr.", "Henry", "White", "henry.white@mail.com", True, password, None)
-            assert staff.check_password(password)
-    
+    # UNIT TEST - #6: SET PASSWORD
+    def test_unit_06_set_password(self):
+        password = "lucaspass"
+        staff = Staff("Prof.", "Lucas", "Garcia", "lucas.garcia@mail.com", True, password, None)
+        assert staff.password != password
+
+    # UNIT TEST - #7: CHECK PASSWORD
+    def test_unit_07_check_password(self):
+        password = "henrypass"
+        staff = Staff("Mr.", "Henry", "White", "henry.white@mail.com", True, password, None)
+        assert staff.check_password(password)
 
 '''
     Integration Tests
 '''
-class staffsIntegrationTests(unittest.TestCase):
+class StaffsIntegrationTests(unittest.TestCase):
 
-    # INTEGRATION TEST - #1: TESTS THE AUTHENTICATION OF STAFF
+    # INTEGRATION TEST - #1: TESTS THE AUTHENTICATION SUCCESS
     def test_integration_01_authenticate_staff_valid(self):
         new_staff = create_staff("Mr.", "Johnny", "Applesauce", "johnny.applesauce@mail.com", True, "johnnypass", None)
-        response = login("johnny.applesauce@mail.com", "johnnypass")
+        response = login(new_staff.email, "johnnypass")
         assert response is not None
 
-    # INTEGRATION TEST - #2: TESTS THE AUTHENTICATION FAILURE OF STAFF
+    # INTEGRATION TEST - #2: TESTS AUTHENTICATION FAILURE
     def test_integration_02_authenticate_staff_invalid(self):
         new_staff = create_staff("Dr.", "Emily", "Chen", "emily.chen@mail.com", True, "emilypass", None)
-        response = login("emily.chen@mail.com", "wrongpassword")
+        response = login(new_staff.email, "notemilypass")
         assert response is None
 
-    # INTEGRATION TEST - #3: TESTS THE CREATION OF STAFF BY AN ADMIN ***
+    # INTEGRATION TEST - #3: CREATE A NEW STAFF
     def test_integration_03_create_staff(self):
         new_admin = create_staff("Ms.", "David", "Lee", "david.lee@mail.com", True, "davidpass", None)
-        new_staff = create_staff("Prof.", "Sophia", "Patel", "sophia.patel@mail.com", False, "sophiypass", new_admin.id)
-
-        assert new_admin is not None
-        assert new_staff is not None
-
-        fetched_staff = get_staff(new_staff.id)
-        self.assertIsNotNone(fetched_staff)
+        new_staff = create_staff("Prof.", "Sophia", "Patel", "sophia.patel@mail.com", False, "sophiapass", new_admin.id)
+        assert new_staff.email == "sophia.patel@mail.com"
         
-    # INTEGRATION TEST - #4: TESTS THE CREATION/ADDING OF A NEW STUDENT ***
+    # INTEGRATION TEST - #4: ADD A NEW STUDENT
     def test_integration_04_add_student(self):
-        new_admin = create_staff("Prof.", "Jackson", "Wang", "jackson.wang@mail.com", True, "jacksonpass", None)
-
-        #ENSURING A STAFF CAN CREATE A STUDENT
         new_student = add_student("816000001","Ava", "Brown", "ava.brown@mail.com")
-        assert new_student is not None
+        assert new_student.email == "ava.brown@mail.com"
 
-        fetched_student = get_student(816000001)
-        self.assertIsNotNone(fetched_student)
-
-    # INTEGRATION TEST - #5: TESTS THE SEARCH OF AN EXISTING STUDENT
+    # INTEGRATION TEST - #5: SEARCHING AN EXISTING STUDENT
     def test_integration_05_get_student_json(self):
-        new_admin = create_staff("Prof.", "Julian", "Lee", "julian.lee@mail.com", True, "julianpass", None)
         new_student = add_student("816000003","Ethan", "Hall", "ethan.hall@mail.com")
-
-        response = get_student_json(816000003)
+        response = get_student_json(new_student.student_id)
         self.assertDictEqual({"student_id": 816000003,
                                "firstname": "Ethan", 
                                "lastname": "Hall", 
                                "email": "ethan.hall@mail.com",
                                "reviews": []}, response)
-        
-    # INTEGRATION TEST - #6: TESTS THE ADDING OF REVIEW FOR A STUDENT ***
+
+    # INTEGRATION TEST - #6: REVIEWING A STUDENT
     def test_integration_06_add_student_reviews(self):
-        # Same as above
         new_admin = create_staff("Ms.", "Sophia", "Taylor", "sophia.taylor@mail.com", True, "sophiapass", None)
         new_staff = create_staff("Mr.", "Oliver", "Martin", "oliver.martin@mail.com", False, "oliverpass", new_admin.id)
         new_student = add_student("816000004","Mia", "Davis", "mia.davis@mail.com")
+        new_review = add_review(new_student.student_id, "Very Good Student", 5, new_staff.id)
+        assert new_review.text == "Very Good Student"
 
-        #ENSURING AN ADMIN ADD REVIEW TO A STUDENT ***
-        response1 = add_review("816000004", "Great student", 5, new_admin.id)
-        assert(len(new_admin.reviews) == 1) # Huh? What if this same admin made multiple reviews towards other students?
-        self.assertEqual(response1.student_id, 816000004)
-        self.assertEqual(response1.text, "Great student")
-        self.assertEqual(response1.rating, 5)
-        self.assertEqual(response1.reviewer_id, new_admin.id)
-
-        #ENSURING A STAFF CAN ADD REVIEW TO A STUDENT ***
-        response2 = add_review("816000004", "Bad student", 1, new_staff.id)
-        assert(len(new_staff.reviews) == 1) 
-        self.assertEqual(response2.student_id, 816000004)
-        self.assertEqual(response2.text, "Bad student")
-        self.assertEqual(response2.rating, 1)
-        self.assertEqual(response2.reviewer_id, new_staff.id)
-
-    # INTEGRATION TEST - #7: TESTS THE RETRIEVAL OF ALL REVIEWS FOR A STUDENT
+    # INTEGRATION TEST - #7: RETRIEVING ALL STUDENT REVIEWS
     def test_integration_07_get_student_reviews_json(self):
         new_admin = create_staff("Dr.", "Evelyn", "Hall", "evelyn.hall@mail.com", True, "evelynpass", None)
         new_staff = create_staff("Ms.", "Ava", "Lee", "ava.lee@mail.com", False, "avapass", new_admin.id)
         new_student = add_student("816000005", "Julian", "Brown", "julian.brown@mail.com")
 
-        add_review("816000005", "Great student", 5, new_admin.id)
-        add_review("816000005", "Bad student", 1, new_staff.id)
+        add_review(new_student.student_id, "Very Good Student", 5, new_admin.id)
+        add_review(new_student.student_id, "Very Bad Student", 1, new_staff.id)
 
-        reviews = get_student_reviews_json(816000005)
+        reviews = get_student_reviews_json(new_student.student_id)
         self.assertListEqual([{"student_id": 816000005,
-                               "text": "Great student", 
+                               "text": "Very Good Student", 
                                "rating": 5, 
                                "reviewer": "Dr. Evelyn Hall"},
-                               
+
                                {"student_id": 816000005,
-                               "text": "Bad student", 
+                               "text": "Very Bad Student", 
                                "rating": 1, 
-                               "reviewer": "Ms. Ava Lee"}], reviews)   
-
-    # INTEGRATION TEST - #8: TESTS THE RETRIEVAL OF ALL REVIEWS FOR A STAFF
-    # Don't really need this but I don't really need a problem with it tbh
-    def test_integration_08_get_staff_reviews_json(self):
-        new_admin = create_staff("Dr.", "Eve", "Hall", "eve.hall@mail.com", True, "evepass", None)
-        new_student1 = add_student("816000006", "Juliana", "Brown", "juliana.brown@mail.com")
-        new_student2 = add_student("816000007", "Llyod", "Green", "llyod.green@mail.com")
-
-        add_review("816000006", "Great student", 5, new_admin.id)
-        add_review("816000007", "Bad student", 1, new_admin.id)
-
-        staff = get_staff(new_admin.id)
-        reviews = staff.reviews
-        reviews = [review.get_json() for review in staff.reviews]
-        self.assertListEqual([{"student_id": 816000006,
-                               "text": "Great student", 
-                               "rating": 5, 
-                               "reviewer": "Dr. Eve Hall"},
-                               
-                               {"student_id": 816000007,
-                                "text": "Bad student", 
-                                "rating": 1, 
-                                "reviewer": "Dr. Eve Hall"}], reviews)
+                               "reviewer": "Ms. Ava Lee"}], reviews)
